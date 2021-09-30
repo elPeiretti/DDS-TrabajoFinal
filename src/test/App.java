0ad00c2c0b87aa229812d6105ad107dba927b433
ElPeiretti <tomaspeiretti@gmail.com>
2021-09-30 16:57:09 -0300

    se hace el MenuBuscarResponsable (es igual al MenuBusquedaPasajero, pero los botones tienen diferente funcionalidad)

--- src/test/App.java
+++ src/test/App.java
@@ -1,5 +1,6 @@
 package test;
 import interfaces.*;
+import interfaces.habitaciones.ocupaciones.*;
 import interfaces.pasajeros.*;
 
 import javax.swing.*;
@@ -9,7 +10,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JFrame ventana = new JFrame();
-		MenuBusquedaPasajero ma = new MenuBusquedaPasajero(ventana);
+		MenuBuscarResponsable ma = new MenuBuscarResponsable(ventana);
 		ventana.setBounds(200,200,655,500);
 		ventana.setVisible(true);
 		
