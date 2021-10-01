c5616a9def34852db7f911c2bb6258d8f5458445
unknown <marianoferesin@gmail.com>
2021-10-01 15:24:34 -0300

    se agregaron clases geograficas y pasajero

--- src/dominio/Ciudad.java
+++ src/dominio/Ciudad.java
@@ -3,5 +3,4 @@ package dominio;
 public class Ciudad {
 	private String nombre;
 	private String idCiudad; 
-
 }
---
+++ src/dominio/Pasajero.java
@@ -0,0 +1,15 @@
+package dominio;
+
+import java.time.Instant;
+
+public class Pasajero {
+	private String nombre;
+	private String apellido;
+	private String CUIT;
+	private String nroDocumento;
+	private Instant fechaDeNacimiento;
+	private String nacionalidad;
+	private String email;
+	private String telefono;
+	private String ocupacion;
+}
