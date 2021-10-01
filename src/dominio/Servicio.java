9c33d09eec6279e5870b40e9bc96ed5fb8306c79
Joaquín <76572032+VoidJ47@users.noreply.github.com>
2021-10-01 15:12:36 -0300

    implementación servicio

---
+++ src/dominio/Servicio.java
@@ -0,0 +1,12 @@
+package dominio;
+
+import java.time.LocalDate;
+
+public class Servicio {
+	private String idServicio;
+	private LocalDate fechaDeRealizacion;
+	private Double precioUnitario;//mal
+	private Integer cantidad;
+	private String descripcion;
+	private Integer cantidadPagada;
+}
