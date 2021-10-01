7ed8a74aa6641919e7299f3e56b4ca14570973cd
Joaquín <76572032+VoidJ47@users.noreply.github.com>
2021-10-01 14:52:10 -0300

    implementación Factura
    Factura, Pago, ResponsablePagoTercero

---
+++ src/dominio/ResponsablePagoTercero.java
@@ -0,0 +1,8 @@
+package dominio;
+
+public class ResponsablePagoTercero {
+	private String idResponsable;
+	private String razonSocial;
+	private String cuit;
+	private String telefono;
+}
---
+++ src/dominio/TipoFactura.java
@@ -0,0 +1,5 @@
+package dominio;
+
+public enum TipoFactura {
+	A,B
+}
