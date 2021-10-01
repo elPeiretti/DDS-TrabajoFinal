3b448c4c6dd8b27efd76303db93a93fe15246132
juan312 <juanalbani48@gmail.com>
2021-10-01 16:33:03 -0300

    DatePicker Agregado

--- src/interfaces/pasajeros/MenuAltaPasajero.java
+++ src/interfaces/pasajeros/MenuAltaPasajero.java
@@ -2,7 +2,13 @@ package interfaces.pasajeros;
 
 import javax.swing.*;
 
+import com.toedter.calendar.JCalendar;
+import com.toedter.calendar.JDateChooser;
+import com.toedter.calendar.JSpinnerDateEditor;
+
 import interfaces.misc.Encabezado;
+
+
 
 public class MenuAltaPasajero extends JPanel {
 
@@ -12,7 +18,6 @@ public class MenuAltaPasajero extends JPanel {
 	private JTextField tf_cuit;
 	private JTextField tf_numero_documento;
 	private JTextField tf_calle;
-	private JTextField tf_nacimiento;
 	private JTextField tf_numero_direccion;
 	private JTextField tf_ocupacion;
 	private JTextField tf_codigo_postal;
@@ -20,6 +25,7 @@ public class MenuAltaPasajero extends JPanel {
 	private JTextField tf_departamento_direccion;
 	private JTextField tf_telefono;
 	private JPanel p_encabezado;
+	private JDateChooser fecha_nacimiento;
 	
 	public MenuAltaPasajero(JFrame ventana_contenedora) {
 		this.ventana_contenedora = ventana_contenedora;
@@ -30,13 +36,13 @@ public class MenuAltaPasajero extends JPanel {
 		add(tf_apellido_pasajero);
 		tf_apellido_pasajero.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel("Apellido (*)");
-		lblNewLabel.setBounds(23, 108, 68, 14);
-		add(lblNewLabel);
+		JLabel lbl_apellido = new JLabel("Apellido (*)");
+		lbl_apellido.setBounds(23, 108, 68, 14);
+		add(lbl_apellido);
 		
-		JLabel lblNewLabel_1 = new JLabel("Nombres (*)");
-		lblNewLabel_1.setBounds(23, 139, 68, 14);
-		add(lblNewLabel_1);
+		JLabel lbl_nombres = new JLabel("Nombres (*)");
+		lbl_nombres.setBounds(23, 139, 68, 14);
+		add(lbl_nombres);
 		
 		tf_nombres_pasajero = new JTextField();
 		tf_nombres_pasajero.setColumns(10);
@@ -48,21 +54,21 @@ public class MenuAltaPasajero extends JPanel {
 		tf_cuit.setBounds(111, 167, 122, 20);
 		add(tf_cuit);
 		
-		JLabel lblNewLabel_1_1 = new JLabel("CUIT");
-		lblNewLabel_1_1.setBounds(23, 170, 68, 14);
-		add(lblNewLabel_1_1);
+		JLabel lbl_cuit = new JLabel("CUIT");
+		lbl_cuit.setBounds(23, 170, 68, 14);
+		add(lbl_cuit);
 		
 		JComboBox cb_paises = new JComboBox();
 		cb_paises.setBounds(111, 245, 122, 24);
 		add(cb_paises);
 		
-		JLabel lblNewLabel_1_1_1 = new JLabel("Pais (*)");
-		lblNewLabel_1_1_1.setBounds(23, 250, 68, 14);
-		add(lblNewLabel_1_1_1);
+		JLabel lbl_pais = new JLabel("Pais (*)");
+		lbl_pais.setBounds(23, 250, 68, 14);
+		add(lbl_pais);
 		
-		JLabel lblNewLabel_1_1_1_1 = new JLabel("Ciudad (*)");
-		lblNewLabel_1_1_1_1.setBounds(23, 285, 68, 14);
-		add(lblNewLabel_1_1_1_1);
+		JLabel lbl_ciudad = new JLabel("Ciudad (*)");
+		lbl_ciudad.setBounds(23, 285, 68, 14);
+		add(lbl_ciudad);
 		
 		JComboBox cb_ciudades = new JComboBox();
 		cb_ciudades.setBounds(111, 280, 122, 24);
@@ -72,13 +78,13 @@ public class MenuAltaPasajero extends JPanel {
 		cb_provincias.setBounds(495, 245, 122, 24);
 		add(cb_provincias);
 		
-		JLabel lblNewLabel_1_1_1_2 = new JLabel("Provincia (*)");
-		lblNewLabel_1_1_1_2.setBounds(346, 250, 68, 14);
-		add(lblNewLabel_1_1_1_2);
+		JLabel lbl_provincia = new JLabel("Provincia (*)");
+		lbl_provincia.setBounds(346, 250, 68, 14);
+		add(lbl_provincia);
 		
-		JLabel lblNewLabel_1_1_1_2_1 = new JLabel("Tipo de Documento (*)");
-		lblNewLabel_1_1_1_2_1.setBounds(346, 108, 139, 14);
-		add(lblNewLabel_1_1_1_2_1);
+		JLabel lbl_tipo_documento = new JLabel("Tipo de Documento (*)");
+		lbl_tipo_documento.setBounds(346, 108, 139, 14);
+		add(lbl_tipo_documento);
 		
 		JComboBox cb_tipos_documento = new JComboBox();
 		cb_tipos_documento.setBounds(495, 101, 122, 24);
@@ -89,48 +95,43 @@ public class MenuAltaPasajero extends JPanel {
 		tf_numero_documento.setBounds(495, 139, 122, 20);
 		add(tf_numero_documento);
 		
-		JLabel lblNewLabel_1_2 = new JLabel("Numero de Documento (*)");
-		lblNewLabel_1_2.setBounds(346, 142, 139, 14);
-		add(lblNewLabel_1_2);
+		JLabel lbl_numero_documento = new JLabel("Numero de Documento (*)");
+		lbl_numero_documento.setBounds(346, 142, 139, 14);
+		add(lbl_numero_documento);
 		
-		JLabel lblNewLabel_1_1_1_2_2 = new JLabel("Factura (*)");
-		lblNewLabel_1_1_1_2_2.setBounds(346, 173, 68, 14);
-		add(lblNewLabel_1_1_1_2_2);
+		JLabel lbl_factura = new JLabel("Factura (*)");
+		lbl_factura.setBounds(346, 173, 68, 14);
+		add(lbl_factura);
 		
 		JComboBox cb_factura = new JComboBox();
 		cb_factura.setBounds(495, 170, 122, 24);
 		add(cb_factura);
 		
-		JLabel lblNewLabel_1_1_2 = new JLabel("Calle (*)");
-		lblNewLabel_1_1_2.setBounds(23, 336, 84, 14);
-		add(lblNewLabel_1_1_2);
+		JLabel lbl_calle = new JLabel("Calle (*)");
+		lbl_calle.setBounds(23, 336, 84, 14);
+		add(lbl_calle);
 		
 		tf_calle = new JTextField();
 		tf_calle.setColumns(10);
 		tf_calle.setBounds(111, 333, 122, 20);
 		add(tf_calle);
 		
-		JLabel lblNewLabel_1_1_2_1 = new JLabel("Nacimiento (*)");
-		lblNewLabel_1_1_2_1.setBounds(23, 201, 84, 14);
-		add(lblNewLabel_1_1_2_1);
+		JLabel lbl_nacimiento = new JLabel("Nacimiento (*)");
+		lbl_nacimiento.setBounds(23, 201, 84, 14);
+		add(lbl_nacimiento);
 		
-		tf_nacimiento = new JTextField();
-		tf_nacimiento.setColumns(10);
-		tf_nacimiento.setBounds(111, 198, 122, 20);
-		add(tf_nacimiento);
-		
-		JLabel lblNewLabel_1_1_2_2 = new JLabel("Numero (*)");
-		lblNewLabel_1_1_2_2.setBounds(23, 381, 84, 14);
-		add(lblNewLabel_1_1_2_2);
+		JLabel lbl_numero = new JLabel("Numero (*)");
+		lbl_numero.setBounds(23, 381, 84, 14);
+		add(lbl_numero);
 		
 		tf_numero_direccion = new JTextField();
 		tf_numero_direccion.setColumns(10);
 		tf_numero_direccion.setBounds(111, 378, 122, 20);
 		add(tf_numero_direccion);
 		
-		JLabel lblNewLabel_1_1_2_2_1 = new JLabel("Ocupacion (*)");
-		lblNewLabel_1_1_2_2_1.setBounds(23, 412, 84, 14);
-		add(lblNewLabel_1_1_2_2_1);
+		JLabel lbl_ocupacion = new JLabel("Ocupacion (*)");
+		lbl_ocupacion.setBounds(23, 412, 84, 14);
+		add(lbl_ocupacion);
 		
 		tf_ocupacion = new JTextField();
 		tf_ocupacion.setColumns(10);
@@ -145,36 +146,36 @@ public class MenuAltaPasajero extends JPanel {
 		btn_siguiente.setBounds(526, 447, 91, 23);
 		add(btn_siguiente);
 		
-		JLabel lblNewLabel_1_1_2_3 = new JLabel("Codigo Postal (*)");
-		lblNewLabel_1_1_2_3.setBounds(346, 285, 84, 14);
-		add(lblNewLabel_1_1_2_3);
+		JLabel lbl_codigo_postal = new JLabel("Codigo Postal (*)");
+		lbl_codigo_postal.setBounds(346, 285, 84, 14);
+		add(lbl_codigo_postal);
 		
 		tf_codigo_postal = new JTextField();
 		tf_codigo_postal.setColumns(10);
 		tf_codigo_postal.setBounds(495, 282, 122, 20);
 		add(tf_codigo_postal);
 		
-		JLabel lblNewLabel_1_1_2_3_1 = new JLabel("Piso");
-		lblNewLabel_1_1_2_3_1.setBounds(346, 336, 30, 14);
-		add(lblNewLabel_1_1_2_3_1);
+		JLabel lbl_piso = new JLabel("Piso");
+		lbl_piso.setBounds(346, 336, 30, 14);
+		add(lbl_piso);
 		
 		tf_piso_direccion = new JTextField();
 		tf_piso_direccion.setColumns(10);
 		tf_piso_direccion.setBounds(386, 333, 44, 20);
 		add(tf_piso_direccion);
 		
-		JLabel lblNewLabel_1_1_2_3_1_1 = new JLabel("Departamento");
-		lblNewLabel_1_1_2_3_1_1.setBounds(457, 336, 71, 14);
-		add(lblNewLabel_1_1_2_3_1_1);
+		JLabel lbl_departamento = new JLabel("Departamento");
+		lbl_departamento.setBounds(457, 336, 71, 14);
+		add(lbl_departamento);
 		
 		tf_departamento_direccion = new JTextField();
 		tf_departamento_direccion.setColumns(10);
 		tf_departamento_direccion.setBounds(538, 333, 79, 20);
 		add(tf_departamento_direccion);
 		
-		JLabel lblNewLabel_1_2_1 = new JLabel("Telefono (*)");
-		lblNewLabel_1_2_1.setBounds(346, 378, 139, 14);
-		add(lblNewLabel_1_2_1);
+		JLabel lbl_telefono = new JLabel("Telefono (*)");
+		lbl_telefono.setBounds(346, 378, 139, 14);
+		add(lbl_telefono);
 		
 		tf_telefono = new JTextField();
 		tf_telefono.setColumns(10);
@@ -185,12 +186,17 @@ public class MenuAltaPasajero extends JPanel {
 		cb_nacionalidad.setBounds(457, 412, 160, 24);
 		add(cb_nacionalidad);
 		
-		JLabel lblNewLabel_1_1_1_2_3 = new JLabel("Nacionalidad (*)");
-		lblNewLabel_1_1_1_2_3.setBounds(346, 417, 84, 14);
-		add(lblNewLabel_1_1_1_2_3);
+		JLabel lbl_nacionalidad = new JLabel("Nacionalidad (*)");
+		lbl_nacionalidad.setBounds(346, 417, 84, 14);
+		add(lbl_nacionalidad);
 		
 		p_encabezado = new Encabezado();
 		p_encabezado.setBounds(0, 0, 640, 80);
 		add(p_encabezado);
+		
+		fecha_nacimiento = new JDateChooser("yyyy/MM/dd", "####/##/##", '_');
+		fecha_nacimiento.setBounds(111,201,122,20);
+		add(fecha_nacimiento);
+		
 	}
 }
