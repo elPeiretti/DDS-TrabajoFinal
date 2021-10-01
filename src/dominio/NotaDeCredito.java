3c903f38202585360a93df549e8243c32b61fd02
Joaquín <76572032+VoidJ47@users.noreply.github.com>
2021-10-01 15:19:18 -0300

    implementación NotaDeCredito

---
+++ src/dominio/NotaDeCredito.java
@@ -0,0 +1,11 @@
+package dominio;
+
+import java.time.Instant;
+
+public class NotaDeCredito {
+	private Instant fechaDeEmision;
+	private TipoNotaDeCredito tipo;
+	private String idNota;
+	private Double monto;//mal
+	private Double totalIVA;//mal
+}
