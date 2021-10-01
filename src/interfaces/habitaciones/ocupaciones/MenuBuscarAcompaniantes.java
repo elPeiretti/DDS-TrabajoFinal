da1e41d2134e0bac3776875b6c9f52cc626f032c
ElPeiretti <tomaspeiretti@gmail.com>
2021-10-01 15:46:01 -0300

    se cambia acompañante por acompaniante

--- src/interfaces/habitaciones/ocupaciones/MenuBuscarAcompaniantes.java
+++ src/interfaces/habitaciones/ocupaciones/MenuBuscarAcompaniantes.java
@@ -2,11 +2,11 @@ package interfaces.habitaciones.ocupaciones;
 
 import javax.swing.*;
 
-public class MenuBuscarAcompañantes extends JPanel {
+public class MenuBuscarAcompaniantes extends JPanel {
 
 	private JFrame ventana_contenedora;
 	
-	public MenuBuscarAcompañantes(JFrame ventana_contenedora) {
+	public MenuBuscarAcompaniantes(JFrame ventana_contenedora) {
 		this.ventana_contenedora = ventana_contenedora;
 	}	
 
