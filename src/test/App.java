27217f4e539b4f4f36e92d62ce25ed8a0bc5c50c
juan312 <juanalbani48@gmail.com>
2021-10-01 16:53:28 -0300

    JCalendar agregado al proyecto

--- src/test/App.java
+++ src/test/App.java
@@ -12,11 +12,9 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JFrame ventana = new JFrame();
-<<<<<<< HEAD
+		
 		JPanel ma = new MenuAltaPasajero(ventana);
-=======
-		JPanel ma = new MenuConfirmarReserva(ventana);
->>>>>>> 00d6f815b2cba28628b4cddb99b6e7b5785734a0
+
 		ventana.setBounds(200,200,655,500);
 		ventana.setVisible(true);
 		
