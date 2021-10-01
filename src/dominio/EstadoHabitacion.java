c5e77bb0a19e1788528b2d113029d778485fc180
Joaquín <76572032+VoidJ47@users.noreply.github.com>
2021-10-01 16:02:35 -0300

    implementacion reservas

---
+++ src/dominio/EstadoHabitacion.java
@@ -0,0 +1,5 @@
+package dominio;
+
+public enum EstadoHabitacion {
+	OCUPADA,RESERVADA,LIBRE,MANTENIMIENTO
+}
---
+++ src/dominio/Habitacion.java
@@ -0,0 +1,10 @@
+package dominio;
+
+import java.util.List;
+
+public class Habitacion {
+	private String numbero;
+	private EstadoHabitacion estado;
+	private String idHabitacion;
+	private List<Reserva> reservas;
+}
