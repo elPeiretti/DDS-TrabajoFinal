ee6d32b12e095820fa8c617047c2e902babc4558
Joaquín <76572032+VoidJ47@users.noreply.github.com>
2021-10-05 16:11:51 -0300

    agregar cositas que me olvidé

--- src/interfaces/facturacion/MenuConsumosPorHabitacion.java
+++ src/interfaces/facturacion/MenuConsumosPorHabitacion.java
@@ -15,6 +15,12 @@ public class MenuConsumosPorHabitacion extends JPanel {
 	private JButton jb_cancelar;
 	private ResultPane rp_facturas;
 	private JLabel lbl_nom_resp;
+	private JLabel lbl_subtotal_tag;
+	private JLabel lbl_subtotal;
+	private JLabel lbl_iva;
+	private JLabel lbl_total;
+	private JLabel lbl_iva_tag;
+	private JLabel lbl_total_tag;
 	
 	public MenuConsumosPorHabitacion(JFrame ventana_contenedora) {
 	
@@ -41,12 +47,42 @@ public class MenuConsumosPorHabitacion extends JPanel {
 		add(jb_cancelar);
 		
 		rp_facturas = new ResultPane();
-		rp_facturas.setBounds(10, 156, 620, 254);
+		rp_facturas.setBounds(10, 156, 620, 184);
 		add(rp_facturas);
 		
 		lbl_nom_resp = new JLabel("<cargar nombre>");
 		lbl_nom_resp.setHorizontalAlignment(SwingConstants.LEFT);
 		lbl_nom_resp.setBounds(124, 121, 506, 14);
 		add(lbl_nom_resp);
+		
+		lbl_subtotal_tag = new JLabel("Subtotal:");
+		lbl_subtotal_tag.setHorizontalAlignment(SwingConstants.RIGHT);
+		lbl_subtotal_tag.setBounds(419, 351, 104, 14);
+		add(lbl_subtotal_tag);
+		
+		lbl_subtotal = new JLabel("<cargar nombre>");
+		lbl_subtotal.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_subtotal.setBounds(533, 351, 98, 14);
+		add(lbl_subtotal);
+		
+		lbl_iva = new JLabel("<cargar nombre>");
+		lbl_iva.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_iva.setBounds(532, 371, 98, 14);
+		add(lbl_iva);
+		
+		lbl_total = new JLabel("<cargar nombre>");
+		lbl_total.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_total.setBounds(533, 395, 98, 14);
+		add(lbl_total);
+		
+		lbl_iva_tag = new JLabel("IVA(21%)");
+		lbl_iva_tag.setHorizontalAlignment(SwingConstants.RIGHT);
+		lbl_iva_tag.setBounds(419, 371, 104, 14);
+		add(lbl_iva_tag);
+		
+		lbl_total_tag = new JLabel("Monto Total:");
+		lbl_total_tag.setHorizontalAlignment(SwingConstants.RIGHT);
+		lbl_total_tag.setBounds(419, 395, 104, 14);
+		add(lbl_total_tag);
 	}
 }
