1a18593cb4e9ba26b40e2c120c917568c9af7154
juan312 <juanalbani48@gmail.com>
2021-10-08 19:55:32 -0300

    Factura y Pago Mapeados

--- src/main/java/com/tp/dominio/Factura.java
+++ src/main/java/com/tp/dominio/Factura.java
@@ -3,15 +3,35 @@ package com.tp.dominio;
 import java.time.Instant;
 import java.util.Collection;
 
+import javax.persistence.*;
+@Entity
+@Table(name = "tpdds.factura")
+
 public class Factura {
+	@Column (name = "tipo_factura")
 	private TipoFactura tipo;
+	@Column (name =  "importe_factura")
 	private Double importeTotal;//mal
+	@Column (name =  "fecha_de_emision")
 	private Instant fechaDeEmision;
-	private String idFactura;
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@Column (name =  "id_factura")
+	private Integer idFactura;
+	@Column (name =  "total_iva")
 	private Double totalIVA;
+	@OneToOne (mappedBy = "factura")
 	private Pago pago;
+	@ManyToOne
+	@JoinColumn (name =  "id_responsable_tercero", referencedColumnName = "id_responsable_tercero")
 	private ResponsablePagoTercero responsableTercero;
+	@ManyToOne
+	@JoinColumn (name =  "id_pasajero", referencedColumnName = "id_pasajero")
 	private Pasajero responsablePasajero;
+	@OneToMany
+	@JoinColumn (name =  "id_factura", referencedColumnName = "id_factura")
 	private Collection<ItemFactura> items;
+	@ManyToOne
+	@JoinColumn (name =  "id_nota", referencedColumnName = "id_nota")
 	private NotaDeCredito notaCredito;
 }
--- src/main/java/com/tp/dominio/Pago.java
+++ src/main/java/com/tp/dominio/Pago.java
@@ -1,11 +1,25 @@
 package com.tp.dominio;
 
+import java.time.Instant;
 import java.time.LocalDate;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tpdds.pago")
+
 public class Pago {
-	private String idPago;
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@Column (name = "id_pago")
+	private Integer idPago;
+	@Column (name = "monto_acumulado")
 	private Double montoAcumulado;//mal
+	@Column (name = "vuelto")
 	private Double vuelto;//mal
-	private LocalDate fechaDeRealizacion;
+	@Column (name = "fecha_de_realizacion")
+	private Instant fechaDeRealizacion;
+	@OneToOne
+	@JoinColumn(name = "id_factura", referencedColumnName = "id_factura")
 	private Factura factura;
 }
